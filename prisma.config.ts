f3f62b95c956d397e9bb12df6524dbb333e9e793
Veroshan <veroshan@ranged.com.au>
2026-05-23 15:41:20 +1000

    Remove invalid adapter property from prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,6 @@
 // npm install --save-dev prisma dotenv
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
-import { PrismaLibSql } from "@prisma/adapter-libsql";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -11,6 +10,5 @@ export default defineConfig({
   },
   datasource: {
     url: process.env["DATABASE_URL"],
-    adapter: new PrismaLibSql({ url: process.env["DATABASE_URL"]! }),
   },
 });
